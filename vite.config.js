adc87b8312f50396c537e8d0659d7caff9ba9c3a
111nation <tafara.k@outlook.com>
2025-10-21 08:54:52 +0000

    Updated vite config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
+    // Add build
     rollupOptions: {
       input: {
         main: "index.html",
