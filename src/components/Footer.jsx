f58c1f3742ffd3d0d0372d5d10f3acae4a2f9865
111nation <tafara.k@outlook.com>
2025-10-21 10:42:05 +0000

    Updated UI

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,25 +5,31 @@ import { saveConfig } from "../config";
 function Footer(props) {
   const navigate = useNavigate();
   return (
-    <footer className="w-screen bg-dark-green flex items-center justify-between mt-30 p-3 px-7 fixed bottom-0 left-0">
-      <Button
-        onClick={() => {
-          navigate(props.prev);
-          props.onPrev();
-          saveConfig();
-        }}
-      >
-        ← PREV
-      </Button>
-      <Button
-        onClick={() => {
-          navigate(props.next);
-          props.onNext();
-          saveConfig();
-        }}
-      >
-        NEXT →
-      </Button>
+    <footer
+      className={`w-screen bg-dark-green flex items-center ${props.prev ? "justify-between" : "justify-end"} mt-30 p-3 px-7 fixed bottom-0 left-0`}
+    >
+      {props.prev && (
+        <Button
+          onClick={() => {
+            navigate(props.prev);
+            props.onPrev();
+            saveConfig();
+          }}
+        >
+          ← PREV
+        </Button>
+      )}
+      {props.next && (
+        <Button
+          onClick={() => {
+            navigate(props.next);
+            props.onNext();
+            saveConfig();
+          }}
+        >
+          NEXT →
+        </Button>
+      )}
     </footer>
   );
 }
